1be6dbb6c713c662171d66921de84f28662feecc
Nathan <nluskcodes@gmail.com>
2025-02-19 23:49:21 -0800

    Refine default colorscheme; add dark scheme; add .xbindkeys file

--- config.lisp
+++ config.lisp
@@ -42,20 +42,43 @@
 
 ;; Colorscheme
 (define-configuration browser
-  ((theme
-    (make-instance 'theme:theme
-      :background-color "black"
-      :background-color+ "#00ff00"
-      :background-color- "#00FF00"
-      :on-background-color "white"    
-      :action-color "#FF0000"
-      :primary-color "#0000FF"
-      :secondary-color "#080808"
-      :text-color "white"
-      :contrast-text-color "black"))))
+  ((theme default)))
 
-; Call xbindkeys using xdotool to use y and u in place of j and k for vi mode
-; Was unable to get vi mode scrolling working on certain modern sites with nyxt defaults
-; Fixed by mapping mod4 + y and u to scroll wheel up and down (click 4 and 5 in xdotool)
-; This hack works as a temp fix for the issue and works well with no issues
+;; Default theme
+(defvar default
+  (make-instance 'theme:theme
+    :background-color "#000000"
+    :background-color+ "#ff0000"
+    :background-color- "#00ff00"
+    :action-color "#af00ff"
+    :primary-color "#0000ff"
+    :secondary-color "#080808"
+    :highlight-color "af00ff"
+    :highlight-color+ "af00ff"
+    :highlight-color- "af00ff"
+    :text-color "#ffffff"
+    :contrast-text-color "#000000"))
+
+;; Dark theme
+(defvar my_dark
+  (make-instance 'theme:theme
+    :background-color "#000000"
+    :background-color+ "#333333"
+    :background-color- "#666666"
+    :action-color "#999999"
+    :primary-color "#1a1a1a"
+    :secondary-color "#080808"
+    :text-color "#f2f2f2"
+    :contrast-text-color "#000000"))
+
+;; Set colorscheme
+;; Define colorscheme above and plug variable in here
+(define-configuration browser ((theme default)))
+
+;; Fix vi mode scrolling
+;; Call xbindkeys using xdotool to use y and u in place of j and k for vi mode
+;; Was unable to get vi mode scrolling working on certain modern sites with nyxt defaults
+;; Fixed by mapping mod4 + y and u to scroll wheel up and down (click 4 and 5 in xdotool)
+;; This hack works as a temp fix for the issue and works well with no issues
+;; Check .xbindkeysrc for codes (.xbindkeysrc should be placed in ~/ dir)
 (uiop:run-program '("xbindkeys"))
